8adedae5cc54a50456f3238a8300d8fe9a3c00f1
Seth Djkstra <Seth.d87@icloud.com>
2023-02-03 18:35:55 -0700

    Add project files.

---
+++ Appliances/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Appliances
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
