05cfd9fe44250e7864fbac9a16aa85d94e7e5a6d
Seth Djkstra <Seth.d87@icloud.com>
2023-02-03 19:02:19 -0700

    Made menu directory, added new classes (Empty) to the project.

--- Appliances/Program.cs
+++ Appliances/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("                                               " +
+                "Welcome to Modern Appliances");
+            Console.WriteLine("                                               " +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            Console.WriteLine("\n   How may we assist you?\n   ----------------------");
+            Console.WriteLine(" 1 - Check out Appliance");
+            Console.WriteLine(" 2 - Find appliances by brand");
+            Console.WriteLine(" 3 - Display appliances by type");
+            Console.WriteLine(" 4 - Produce random appliance list");
+            Console.WriteLine(" 5 - Save & exit");
+
+            Console.Write("\nEnter Option: ");
+
+            int userInput = int.Parse(Console.ReadLine());
         }
     }
 }
