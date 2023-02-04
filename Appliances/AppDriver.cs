a546bd90b620f82067183b75b127d7fd5699710b
Seth Djkstra <Seth.d87@icloud.com>
2023-02-03 19:37:43 -0700

    Made appDriver, have it setup to read appliance.txt file. Finished Refrigerator class, inherited from appliances

---
+++ Appliances/AppDriver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appliances
+{
+    class AppDriver
+    {
+        public List<Appliance> employees = new List<Appliance>();
+
+        private void loadingFile()
+        {
+            string filepath = @"C:\Users\Seth\source\repos\Appliances\Appliances\appliances.txt";
+            string[] lines = File.ReadAllLines(filepath);
+        }
+    }
+}
--- Appliances/Refrigerator.cs
+++ Appliances/Refrigerator.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Appliances
 {
-    class Refrigerator
+    class Refrigerator : Appliance
     {
+        private int doors;
+        private int height;
+        private int width;
+
+        public Refrigerator(int itemNum, string brand, int quantity, int wattage, 
+            string colour, double price, int doors, int height, int width) : base(itemNum, brand, quantity, wattage, colour, price)
+        {
+            this.doors = itemNum;
+            this.height = height;
+            this.width = width;
+        }
+
+        public int getDoors()
+        {
+            return doors;
+        }
+        public int getHeight()
+        {
+            return height;
+        }
+        public int getWidth()
+        {
+            return width;
+        }
+
+        public void setDoors(int doors)
+        {
+            this.doors = doors;
+        }
+        public void setHeight(int height)
+        {
+            this.height = height;
+        }
+        public void setWidth(int width)
+        {
+            this.width = width;
+        }
+
+        public override string ToString()
+        {
+            return "Doors: "+ doors +
+                "Height (inches): " + height +
+                "Width (inches): " + width;
+        }
     }
 }
