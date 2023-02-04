0e8a45a9bae6ddeb7cbd772ab2fa1b5cc4cd0abb
Seth Djkstra <Seth.d87@icloud.com>
2023-02-03 20:02:27 -0700

    Completed the dishwasher class

--- Appliances/Dishwasher.cs
+++ Appliances/Dishwasher.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Appliances
 {
-    internal class Dishwasher
+    class Dishwasher : Appliance
     {
+        private string sound;
+        private string feature;
+
+        public Dishwasher(int itemNum, string brand, int quantity, int wattage,
+            string colour, double price, string feature, string sound) :
+            base(itemNum, brand, quantity, wattage, colour, price)
+        {
+            this.sound = sound;
+            this.feature = feature;
+        }
+
+        public string getSound()
+        {
+            return sound;
+        }
+        public string getFeature()
+        {
+            return feature;
+        }
+        public void setSound(string sound)
+        {
+            this.sound = sound;
+        }
+        public void setFeature(string feature)
+        {
+            this.feature = feature;
+        }
+
+        public override string ToString()
+        {
+            return "Sound Rating: " + sound +
+                "Feature and Finish: " + feature;
+        }
     }
 }
