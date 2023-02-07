a325f558ee6b057412b204773a524777b25cc877
Seth Djkstra <Seth.d87@icloud.com>
2023-02-07 11:51:42 -0700

    Moved menu functions to appDriver class

--- Appliances/AppDriver.cs
+++ Appliances/AppDriver.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace Appliances
 {
     class AppDriver
-    {
+
+    {   
         public List<Appliance> appliaces = new List<Appliance>();
 
         private void loadingFile()
@@ -28,6 +29,24 @@ namespace Appliances
                 }
 
             }
+            
+        }
+        public AppDriver()
+        {
+            Console.WriteLine("                                               " +
+                "Welcome to Modern Appliances");
+            Console.WriteLine("                                               " +
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            Console.WriteLine("\n   How may we assist you?\n   ----------------------");
+            Console.WriteLine(" 1 - Check out Appliance");
+            Console.WriteLine(" 2 - Find appliances by brand");
+            Console.WriteLine(" 3 - Display appliances by type");
+            Console.WriteLine(" 4 - Produce random appliance list");
+            Console.WriteLine(" 5 - Save & exit");
+
+            Console.Write("\nEnter Option: ");
+            int userInput = int.Parse(Console.ReadLine());
         }
     }
 }
