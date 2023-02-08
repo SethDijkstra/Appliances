aa5021cae8f0716b3e9cce9fdda824e494a9d676
Seth_Dijkstra <119450434+SethDijkstra@users.noreply.github.com>
2023-02-08 09:39:07 -0700

    Update AppDriver.cs

--- Appliances/AppDriver.cs
+++ Appliances/AppDriver.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,14 @@ namespace Appliances
 {
     class AppDriver
 
-    {   
-        public List<Appliance> appliaces = new List<Appliance>();
+    {
+        
+        public List<Appliance> appliances = new List<Appliance>();
+        
 
         private void loadingFile()
         {
-            string filepath = @"C:\Users\Seth\source\repos\Appliances\Appliances\appliances.txt";
+            string filepath = @"/Users/sethdijkstra/Library/Mobile Documents/com~apple~CloudDocs/C#/Appliances-master/Appliances/appliance.txt";
             string[] lines = File.ReadAllLines(filepath);
 
             foreach (string line in lines)
@@ -24,8 +26,26 @@ namespace Appliances
 
                 if (firstChar == '1')
                 {
-                    appliaces.Add(new Refrigerator(int.Parse(fields[0]), fields[1], int.Parse(fields[2]), int.Parse(fields[3]), fields[4], double.Parse(fields[5]),
-                        int.Parse(fields[6]), int.Parse(fields[7]), int.Parse(fields[8])));
+                    appliances.Add(new Refrigerator(int.Parse(fields[0]), fields[1], int.Parse(fields[2]), int.Parse(fields[3]), fields[4], double.Parse(fields[5]),
+                        int.Parse(fields[6]), double.Parse(fields[7]), double.Parse(fields[8])));
+                }
+
+                else if (firstChar == '2')
+                {
+                    appliances.Add(new Vacuums(int.Parse(fields[0]), fields[1], int.Parse(fields[2]), int.Parse(fields[3]), fields[4], double.Parse(fields[5]),
+                        fields[6], double.Parse(fields[7])));
+                }
+                else if (firstChar == '3')
+                {
+                    appliances.Add(new Microwaves(int.Parse(fields[0]), fields[1], int.Parse(fields[2]), int.Parse(fields[3]), fields[4], double.Parse(fields[5]), double.Parse(fields[6]), fields[7]) );
+                }
+                else if (firstChar == '4' || firstChar == '5')
+                {
+                    appliances.Add(new Dishwasher(int.Parse(fields[0]), fields[1], int.Parse(fields[2]), int.Parse(fields[3]), fields[4], double.Parse(fields[5]), fields[6], fields[7]));
+                }
+                else
+                {
+                    Console.WriteLine("No appliance with this ID is avaliable.");
                 }
 
             }
@@ -33,20 +53,55 @@ namespace Appliances
         }
         public AppDriver()
         {
-            Console.WriteLine("                                               " +
-                "Welcome to Modern Appliances");
-            Console.WriteLine("                                               " +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            loadingFile();
+            
+            foreach (Appliance appliance in appliances)
+            {
+                Console.WriteLine(appliance);
+            }
+        }
 
-            Console.WriteLine("\n   How may we assist you?\n   ----------------------");
-            Console.WriteLine(" 1 - Check out Appliance");
-            Console.WriteLine(" 2 - Find appliances by brand");
-            Console.WriteLine(" 3 - Display appliances by type");
-            Console.WriteLine(" 4 - Produce random appliance list");
-            Console.WriteLine(" 5 - Save & exit");
 
-            Console.Write("\nEnter Option: ");
-            int userInput = int.Parse(Console.ReadLine());
+        public void displayMenu()
+        {
+            loadingFile();
+            int choice = 0;
+            while (choice != 5)
+            {
+                Console.WriteLine("                                               " +
+                    "Welcome to Modern Appliances");
+                Console.WriteLine("                                               " +
+                    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+                Console.WriteLine("\n   How may we assist you?\n   ----------------------");
+                Console.WriteLine(" 1 - Check out Appliance");
+                Console.WriteLine(" 2 - Find appliances by brand");
+                Console.WriteLine(" 3 - Display appliances by type");
+                Console.WriteLine(" 4 - Produce random appliance list");
+                Console.WriteLine(" 5 - Save & exit");
+
+                Console.Write("\nEnter Option: ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Function 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Function 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("Function 3");
+                        break;
+                    case 4:
+                        Console.WriteLine("Function 4");
+                        break;
+                    case 5:
+                        Console.WriteLine("Thank-You for visiting, Have a nice day!");
+                        break;
+                }
+
+            }
         }
     }
 }
