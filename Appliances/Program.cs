b9abce1efbd18408dfcf56f2060168872ac36970
Seth Djkstra <Seth.d87@icloud.com>
2023-02-13 14:18:03 -0700

    Final Product Code

--- Appliances/Program.cs
+++ Appliances/Program.cs
@@ -2,7 +2,16 @@
 {
     internal class Program
     {
-        //Main method that initializes AppDriver and excecutes all code inside AppDriver
+        /*Appliance Menu (inheritence Example project)
+         * Author(s): Seth, Isaac and Jesse
+         * Date Last Modified: 2023-02-13
+         * Group 2: Winter23_CPRG211_C
+         * 
+         * This code offers users an area to find appliances, options are given to them to search by ID or type or brand. They can also look at random appliances.
+         * This code uses inheritence and Appliance.cs is the superclass in the hierarchy.
+         * 
+         * Main method that initializes AppDriver and excecutes all code inside AppDriver
+         */
         static void Main(string[] args)
         {
             new AppDriver();
